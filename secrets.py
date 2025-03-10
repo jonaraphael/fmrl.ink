ed15c35a53e0468573ebde7c5339d0569b949058
Jona <jona.raphael@gmail.com>
2025-03-09 21:29:23 -0400

    debug

--- secrets.py
+++ secrets.py
@@ -1,19 +1,23 @@
 import os
 
+import google.auth
 from google.cloud import secretmanager
 
 
 def get_secret(secret_id, project_id=None):
     """
     Retrieve the latest version of a secret from Google Cloud Secret Manager.
-    If project_id is not provided, it is taken from the environment variable GCP_PROJECT.
+    If project_id is not provided, try to get it from the environment or default credentials.
     """
     if project_id is None:
         project_id = os.environ.get("GCP_PROJECT")
         if not project_id:
-            raise Exception(
-                "GCP_PROJECT environment variable must be set to use Secret Manager."
-            )
+            try:
+                _, project_id = google.auth.default()
+            except Exception as e:
+                raise Exception(
+                    "GCP_PROJECT must be set or default credentials must be configured."
+                ) from e
 
     client = secretmanager.SecretManagerServiceClient()
     name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
