787f8f75aae938527155603d9ad5894cd7b10b16
Jona <jona.raphael@gmail.com>
2025-03-09 22:05:08 -0400

    specify service account

--- deploy.sh
+++ deploy.sh
@@ -6,27 +6,33 @@ gcloud functions deploy inbound-email \
   --entry-point inbound_email \
   --runtime python39 \
   --trigger-http \
-  --allow-unauthenticated
+  --allow-unauthenticated \
+  --service-account fmrl-sa@fmrl-453205.iam.gserviceaccount.com \
+  --project fmrl-453205
 
 # Deploy cleanup function (triggered by Cloud Scheduler)
 gcloud functions deploy cleanup \
   --entry-point cleanup \
   --runtime python39 \
   --trigger-http \
-  --allow-unauthenticated
+  --allow-unauthenticated \
+  --service-account fmrl-sa@fmrl-453205.iam.gserviceaccount.com \
+  --project fmrl-453205
 
 # Deploy unsubscribe link handler
 gcloud functions deploy unsubscribe-link \
   --entry-point unsubscribe_link \
   --runtime python39 \
   --trigger-http \
-  --allow-unauthenticated
+  --allow-unauthenticated \
+  --service-account fmrl-sa@fmrl-453205.iam.gserviceaccount.com \
+  --project fmrl-453205
 
 # Configure Cloud Scheduler to hit the cleanup endpoint daily.
 gcloud scheduler jobs create http fmrlink-cleanup-job \
   --schedule="0 0 * * *" \
   --http-method=POST \
-  --uri="$(gcloud functions describe cleanup --format='value(httpsTrigger.url)')" \
+  --uri="$(gcloud functions describe cleanup --project fmrl-453205 --format='value(httpsTrigger.url)')" \
   --time-zone="UTC" \
   --attempt-deadline=60s --quiet
 
